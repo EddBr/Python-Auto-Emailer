62feaeffee69cb5cff0809fd50b7bc24716e6535
EddBr <35238776+EddBr@users.noreply.github.com>
2020-08-02 22:58:47 +0200

    Update sensitive.py

--- sensitive.py
+++ sensitive.py
@@ -1,2 +1,3 @@
 recievers = [] #Add email recipients here, DO NOT INCLUDE '@gmail.com'
 sender = [] # Add email bot here, DO NOT INCLUDE '@gmail.com'
+password = "" #Add sender's GMail password here
